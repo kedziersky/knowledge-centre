4d49e0eeee268a66463e56b6f72442c26b215cfe
Kamil <kkedzierski@apptension.com>
2022-11-26 10:23:07 +0100

    Init

---
+++ src/theme/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from "@chakra-ui/react";
+import { globalStyles } from "./styles";
+import { breakpoints } from "./foundations/breakpoints";
+import { buttonStyles } from "./components/button";
+import { badgeStyles } from "./components/badge";
+import { linkStyles } from "./components/link";
+import { drawerStyles } from "./components/drawer";
+import { CardComponent } from "./additions/card/card";
+import { CardBodyComponent } from "./additions/card/cardBody";
+import { CardHeaderComponent } from "./additions/card/cardHeader";
+import { MainPanelComponent } from "./additions/layout/mainPanel";
+import { PanelContentComponent } from "./additions/layout/panelContent";
+import { PanelContainerComponent } from "./additions/layout/panelContainer";
+// import { mode } from "@chakra-ui/theme-tools";
+export default extendTheme(
+  { breakpoints }, // Breakpoints
+  globalStyles,
+  buttonStyles, // Button styles
+  badgeStyles, // Badge styles
+  linkStyles, // Link styles
+  drawerStyles, // Sidebar variant for Chakra's drawer
+  CardComponent, // Card component
+  CardBodyComponent, // Card Body component
+  CardHeaderComponent, // Card Header component
+  MainPanelComponent, // Main Panel component
+  PanelContentComponent, // Panel Content component
+  PanelContainerComponent // Panel Container component
+);
